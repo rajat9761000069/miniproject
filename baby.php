1ae2f99b56b94a480bd9c8b2583603cd3e3fd3ad
rajat9761000069 <rajatchaudhary9359@gmail.com>
2015-11-28 14:25:09 +0530

    Create baby.php

---
+++ baby.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang='en'>
+	
+
+<head>
+<style>
+p {
+    padding: 5px 15px; 
+    border: 1px solid black; 
+	background: blue;
+}  
+h2{
+    padding: 0px 5px 10px 3px; 
+    border: 1px solid black;
+	background: white;
+} 
+</style>
+</head>
+		
+		<meta charset="utf-8" />
+		<title>HOME</title>
+		<link type="text/css" rel="stylesheet" media="screen" href="layout.css"/>
+        </head>
+		<body background="baby.jpg" link="red">
+			<h2 align="center"><font color="blue">             WELCOME TO BABY NAME DATABASE</font></h2>
+			<table id="nav" link="red">
+				<tr link="red">
+					<td></td>
+					<td><p><a href="baby.php" color="red">HOME</a></p></td>
+					<td></td>
+                                        <td><p><a href="topname.php" color="red">TOP NAMES</a></p></td>
+					<td></td>
+					<td><p><a href="graph.php" color="red">POPULARITY CHART</a></p></td>
+					<td></td>
+                                        <td><p><a href="partsrch.php" color="red">PARTIAL SEARCH</a></p></td>
+					<td></td>
+                                        
+		<td></td>
+		</tr>
+		</table>
+		      
+		</body>
+		</html>
